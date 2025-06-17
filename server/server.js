ed4dc96d014a820546d18600c508e887f1bbaf6a
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-06-17 19:20:10 +0530

    Backend  Done 😊

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ import express from "express";
 import dbConnection from "./config/dbConnection.js";
 import dotenv from "dotenv";
 import cors from "cors";
-
 import taskRoutes from "./routes/task.route.js";
+
 const app = express();
 dotenv.config();
 
@@ -11,25 +11,21 @@ const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.DB_URL;
 
 dbConnection(DB_URL);
-
 app.use(express.json());
+
 app.use(
   cors({
     origin: "http://localhost:5173",
   })
 );
 
-// roues
-
 app.use("/api/task",taskRoutes)
-
 
 app.get("/test", (req, res) => {
   res.status(200).json({
     message: "welcome to TodoApp",
   });
 });
-
 app.listen(PORT, () => {
   console.log(`server run at ${PORT} 🟢`);
 });
